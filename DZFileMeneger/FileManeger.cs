72288a35f68cd813b0cfff9a261a5f042b3975cc
User <User@DESKTOP-K15QGHT>
2023-11-07 14:38:54 +0300

    Add project files.

---
+++ DZFileMeneger/FileManeger.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DZFileMeneger
+{
+    internal class FileManeger
+    {
+        private int CountReadRow = 0;
+        private int CountWriteRow = 0;
+        private bool IsStreamWork=true;
+        //streamreader, streamwriter , file.
+        public void BigFileWriter(string path, int countWriteRow, int rowCharCount)
+        {
+            using (StreamWriter streamWriter = new StreamWriter(path, false))
+            {
+                int countRow = countWriteRow / 20;
+                int countTemp = countRow;
+                Console.WriteLine("Write file begin");
+                for (int i = 0; i < countWriteRow; i++)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    for (int j = 0; j < rowCharCount; j++)
+                    {
+                        sb.Append("a");
+                    }
+                    sb.Append("\n");
+                    streamWriter.Write(sb.ToString());
+                    CountWriteRow = i;
+                }
+                IsStreamWork = false;
+                streamWriter.Close();
+                Console.WriteLine("Write file complit");
+            }
+        }
+        public void GetCountWriteRow()
+        {
+            IsStreamWork = true;
+            DateTime time = DateTime.Now;
+            while(IsStreamWork)
+            {
+                Console.WriteLine($"{DateTime.Now - time}ms {CountWriteRow}");
+                Thread.Sleep(200);
+            }
+            Console.WriteLine($"{DateTime.Now - time}ms {CountWriteRow}");
+        }
+        public void GetCountReadRow()
+        {
+            IsStreamWork = true;
+            DateTime time = DateTime.Now;
+            while (IsStreamWork)
+            {
+                Console.WriteLine($"{DateTime.Now - time}ms {CountReadRow}");
+                Thread.Sleep(100);
+            }
+            Console.WriteLine($"{DateTime.Now - time}ms {CountReadRow}");
+        }
+
+        public string BigFileReader(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                Console.WriteLine("Read file begin");
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    CountReadRow++;
+                }
+                string readResult = $"Count is readed row: {CountReadRow}";
+                IsStreamWork = false;
+                Console.WriteLine("Read file complit");
+               // Console.WriteLine(readResult);
+
+                return readResult;
+            }
+
+        }
+    }
+}
---
+++ DZFileMeneger/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DZFileMeneger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileManeger fileManeger = new FileManeger();
+            //создаем потоки записи, чтения и их мониторинга
+            
+            Thread write = new Thread(()=>fileManeger.BigFileWriter("test1", 10000000, 10));
+            Thread writeMonitor = new Thread(fileManeger.GetCountWriteRow);
+            writeMonitor.Start();
+            write.Start();
+            writeMonitor.Join();
+            write.Join();
+            Thread.Sleep(1000);
+            Thread read = new Thread(() => fileManeger.BigFileReader("test1"));
+            Thread readMonitor =new Thread(fileManeger.GetCountReadRow);
+            readMonitor.Start();
+            read.Start();
+            readMonitor.Join();
+            read.Join();
+        }
+    }
+}
